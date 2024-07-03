1a120c778e33491401e352bfc69fb147074257da
Pedro Calderano <pedro.calderano@lmmp.mec.puc-rio.br>
2024-07-03 18:36:32 -0300

    don geomI pin mu

---
+++ examples/geomI2-don/utils.py
@@ -0,0 +1,58 @@
+import jax.numpy as jnp
+import jax
+import numpy as np
+import pickle
+import scipy.io
+
+
+
+def get_coords():
+    dx = .0001
+    x1_max = .021
+    x2_max = .014
+
+    x1 = np.arange(0,.021+dx/2, dx)
+    x2 = np.arange(0,.014+dx/2, dx)
+
+    X = np.zeros((x1.shape[0]*x2.shape[0],2))
+
+    for i in range(x1.shape[0]):
+        X[i*x2.shape[0]:(i+1)*x2.shape[0],:] = np.transpose(np.concatenate(([x1[i]*np.ones(x2.shape[0])],[x2]),axis=0))
+
+    # esquerda p entrada
+    id = np.where(np.squeeze(np.matmul(X, np.array([[1],[0]]))) == 0)[0]
+    inflow_coords = X[id]
+    # direita p saida
+    id = np.where(np.squeeze(np.matmul(X, np.array([[1],[0]]))) == x1_max)[0]
+    outflow_coords = X[id]
+    # bottom
+    id = np.where(np.squeeze(np.matmul(X, np.array([[0],[1]]))) == 0)[0]
+    bot_wall_coords = X[id]
+    # top
+    id = np.where(np.squeeze(np.matmul(X, np.array([[0],[1]]))) == x2_max)[0]
+    top_wall_coords = X[id]
+    # wall = top+bottom
+    wall_coords = np.concatenate((bot_wall_coords, top_wall_coords), axis=0)
+
+    inds = np.where(X[:,1] > x2_max)
+    X = np.delete(X, inds, axis = 0)
+
+    return jax.device_put(X), \
+            jax.device_put(inflow_coords), \
+            jax.device_put(outflow_coords), \
+            jax.device_put(wall_coords)
+
+def get_dataset():
+
+    coords, inflow_coords, outflow_coords, wall_coords = get_coords()
+    mu = [.1, .15]
+    pin = [7, 10.001]
+
+
+    return (
+        coords,
+        inflow_coords,
+        outflow_coords,
+        wall_coords,
+        mu, pin
+    )
